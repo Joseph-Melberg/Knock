e7ec4abf6e0d65c4c4e2f835e733b0a8827f84db
Joseph <jbmelberg@eiu.edu>
2020-05-26 18:11:00 -0500

    Final cleanup finished

--- reader.py
+++ reader.py
@@ -13,7 +13,7 @@ mycursor = mydb.cursor()
 
 mycursor.execute("SELECT date FROM Home_Data.failed_access ORDER BY date desc LIMIT 1")
 latest_str = str(mycursor.fetchall()[0][0])
-print(latest_str)
+#print(latest_str)
 latest = datetime.datetime.strptime(latest_str,'%Y-%m-%d %H:%M:%S')
 
 
@@ -22,7 +22,7 @@ def record(value):
     mycursor.execute(command,value)
     mydb.commit()
   
-    print "done"
+#    print "done"
 # Function to covert string to datetime 
 def convert(date_time): 
     format = '%b %d  %I:%M:%S' # The format 
@@ -33,18 +33,16 @@ done = False
 def find():
 	line = f.readline();
 	if line is '':
-		done = True
 		exit()
-		return
 	res = re.findall(reg,line)
-	for i in res:
-		print(i)
+#	for i in res:
+#		print(i)
 	if(len(res)!=0):
 	#	print line
 		result = list(res[0])
 		result[0] = convert(str(result[0]))
 		if(result[0] > latest):
 			record(result)
-while not done:
+while True:
 	find()
-print("I am finished")
+
